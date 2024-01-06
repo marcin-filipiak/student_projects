0e43737b7083f9006e203a2fe554edf0e64059c2
marcin-filipiak <m.filipiak@noweenergie.org>
2024-01-06 19:51:01 +0000

    test

---
+++ cpp/szablon/kompiluj.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "----Kompilacja programu----"
+g++ -o main main.cpp
+echo "---Kompilacja ukonczona----"
+echo "---Uruchamianie programu---"
+./main
---
+++ cpp/szablon/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"witaj swiecie"<<endl;
+	return 0;
+}
